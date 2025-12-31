b350ac5a4cd4af41af4fc8ec6dcefc3dc6c4282f
Deepak Shenoy <deepak@local>
2025-12-31 00:58:18 -0500

    Initial structure - Data Manager

---
+++ utils/DataManager.h
@@ -0,0 +1,19 @@
+//
+// Created by Deepak Shenoy on 12/31/25.
+//
+
+#ifndef AIRPORT_PASSENGER_FORECASTING_DATAMANAGER_H
+#define AIRPORT_PASSENGER_FORECASTING_DATAMANAGER_H
+
+class DataManager {
+
+    void read_file() {
+
+    }
+
+    int get_file_size() {
+        return 0;
+    }
+};
+
+#endif //AIRPORT_PASSENGER_FORECASTING_DATAMANAGER_H
